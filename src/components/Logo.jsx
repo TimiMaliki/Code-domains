7ac283ea8e1747ead1e671142fe7f880adb62b5b
TimiMaliki <malikitimi@gmail.com>
2023-06-27 00:03:07 +0100

    Progress

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,12 +3,53 @@ import logo from '../assets/style/logo.module.scss'
 const Logo = () => {
     return (
         <div className={logo.logo}>
-            <div className={logo.logoIcon}>
 
+            <div className={logo.logoIcon}>
             </div>
             <div className={logo.logoText}>
-
+             <h4>Free Delivery</h4>
+             <p>Free and Fast Delivery</p>
             </div>
+
+
+            <div className={logo.logoIcon}>
+            </div>
+            <div className={logo.logoText}>
+             <h4>Free Delivery</h4>
+             <p>Free and Fast Delivery</p>
+            </div>
+
+
+
+
+            <div className={logo.logoIcon}>
+            </div>
+            <div className={logo.logoText}>
+             <h4>Free Delivery</h4>
+             <p>Free and Fast Delivery</p>
+            </div>
+
+
+
+            <div className={logo.logoIcon}>
+            </div>
+            <div className={logo.logoText}>
+             <h4>Free Delivery</h4>
+             <p>Free and Fast Delivery</p>
+            </div>
+            
+
+
+
+
+
+
+
+
+
+
+
+
         </div>
     )
 }
