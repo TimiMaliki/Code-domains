928e3921febcdb84a263316c4ab01550a3a664e4
TimiMaliki <malikitimi@gmail.com>
2023-10-02 03:02:34 +0100

    HeroImg

--- src/components/HeroImg.jsx
+++ src/components/HeroImg.jsx
@@ -3,8 +3,9 @@ import Layer from "../assets/heroPics/Layer0-1.png"
 import Vector from "../assets/heroPics/Vector-2.png"
 import Card from  "../assets/heroPics/Card-2.png"
 import Card1 from  "../assets/heroPics/Card-1.png"
+import Card3 from  "../assets/heroPics/Card-3.png"
 import Star1 from  "../assets/heroPics/Star-1.png"
-import Star3 from  "../assets/heroPics/Star-3.png"
+
 const HeroImg = () => {
   return (
     <div className='lg:w-full lg:h-full p-20'>
@@ -13,7 +14,8 @@ const HeroImg = () => {
         <img src={Layer} alt=""  width={350} height={300} style={{borderRadius:"13rem",marginLeft:"8rem" ,}} />
         <img src={Card} alt="" width={150} height={100} style={{marginTop:"-26rem"}}/>
         <img src={Card1} alt="" width={150} height={100} style={{marginTop:"10rem" , marginLeft:"2rem"}}/>
-        <img src={Star1} alt="" style={{marginLeft:"16rem", marginTop:"1rem"}}/>
+        <img src={Star1} alt="" style={{marginLeft:"16rem", marginTop:"2rem"}}/>
+        <img src={Card3} alt="" width={150} height={100} style={{marginLeft:"25rem", marginTop:"-5rem"}}/>
     </div>
   )
 }
