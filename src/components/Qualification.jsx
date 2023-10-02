cb0de871a9857fa09f868c25fc117b9b2778bf05
TimiMaliki <malikitimi@gmail.com>
2023-10-02 16:17:08 +0100

    Qualification

--- src/components/Qualification.jsx
+++ src/components/Qualification.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
 
 const Qualification = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="">
+     
+        <div className="flex justify-center gap-44 text-start w-full  h-full  bg-[#23252A]">
+        <div className="grid place-content-start  text-white ">
+        <div className="flex date">2034-2069</div>
+        <div>
+          <h2>Lorem Ipsum</h2>
+        </div>
+      </div>
 
-export default Qualification
+      <div className="text-white job"><h2 className="text-xl">Senior <br /> Developer</h2></div>
+       <div className="education w-96  h-48 text-white"><h2>
+       A tristique nulla faucibus in quam metus, eget. 
+       Id eleifend arcu consectmetus, eget. Id eleifend arcu consectetur nunc.
+        </h2></div>
+        </div>
+
+     {/* next section */}
+
+     <div className="flex justify-center gap-44 text-start w-full  h-full  bg-[#23252A]">
+        <div className="grid place-content-start  text-white">
+        <div className="flex date">2034-2069</div>
+        <div>
+          <h2>Lorem Ipsum</h2>
+        </div>
+      </div>
+
+      <div className="text-white job"><h2 className="text-xl">Senior <br /> Developer</h2></div>
+       <div className="education w-96  h-48 text-white"><h2>
+       A tristique nulla faucibus in quam metus, eget. 
+       Id eleifend arcu consectmetus, eget. Id eleifend arcu consectetur nunc.
+        </h2></div>
+        </div>
+
+           {/* next section */}
+
+           <div className="flex justify-center gap-44 text-start w-full  h-full  bg-[#23252A]">
+        <div className="grid place-content-start  text-white">
+        <div className="flex date">2034-2069</div>
+        <div>
+          <h2>Lorem Ipsum</h2>
+        </div>
+      </div>
+
+      <div className="text-white job"><h2 className="text-xl">Senior <br /> Developer</h2></div>
+       <div className="education w-96  h-48 text-white"><h2>
+       A tristique nulla faucibus in quam metus, eget. 
+       Id eleifend arcu consectmetus, eget. Id eleifend arcu consectetur nunc.
+        </h2></div>
+        </div>
+
+    </div>
+  );
+};
+
+export default Qualification;
