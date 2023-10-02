35e56788eaa2f49359d2f6ab9bf955e56b7fa432
TimiMaliki <malikitimi@gmail.com>
2023-10-02 03:53:51 +0100

    Logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Layout from "./components/Layout";
+import Logo from "./components/Logo";
 
 function App() {
   return (
     <div>
       <Header />
       <Layout />
+      <Logo/>
     </div>
   );
 }
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import Fiverr from "../assets/LogoPics/Fiverr.png";
+import Github from "../assets/LogoPics/Github.png";
+import Top from "../assets/LogoPics/Top.png";
+import Upwork from "../assets/LogoPics/Upwork.png";
 
 const Logo = () => {
-  return (
-    <div className='flex justify-center items-center p-12' >
-      
-    </div>
-  )
-}
+  return <div className="lg:w-full h-32 bg-blue-900 flex justify-between items-center p-8">
+<img src={Fiverr} alt=""  width={90} height={90}/>
+<img src={Github} alt=""  width={90} height={90}/>
+<img src={Top} alt=""  width={90} height={90}/>
+<img src={Upwork} alt="" width={90} height={90}/>
 
-export default Logo
+  </div>;
+};
+
+export default Logo;
