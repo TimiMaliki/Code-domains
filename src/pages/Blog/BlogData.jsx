182fc4724bc7bc76205b78a9ec08cc2bc39386e7
TimiMaliki <malikitimi@gmail.comgit config --global user.email malikitimi@gmail.com>
2024-03-16 21:31:10 +0100

    added: create-blog component

--- src/pages/Blog/BlogData.jsx
+++ src/pages/Blog/BlogData.jsx
@@ -1,34 +1,48 @@
 import React from "react";
 import { useState } from "react";
 import rectangle from "../../assets/blogpics/Rectangle.png"
+import rectangle13 from "../../assets/blogpics/Rectangle-13.png"
+import rectangleOrange from "../../assets/blogpics/Rectangle-orange.png"
+import rectangle16 from "../../assets/blogpics/Rectangle-16.png"
+import rectangle14 from "../../assets/blogpics/Rectangle-14.png"
+import rectangle15 from "../../assets/blogpics/Rectangle-15.png"
+import rectangle17 from "../../assets/blogpics/Rectangle-bg.png"
+import rectangle18 from "../../assets/blogpics/Rectangle-18.png"
+import rectangle19 from "../../assets/blogpics/Rectangle-19.png"
+import rectangle20 from "../../assets/blogpics/RectangleFace.png"
+import rectangle21 from "../../assets/blogpics/Rectangle-21.png"
+import rectangle22 from "../../assets/blogpics/Rectangle-22.png"
+
+
+
 
 
 const CreateBlog = () => {
     const [blogs, setBlogs] = useState([
         { icon: rectangle, title: "Here are some things you should know regarding how we work", id: 1 },
-        { icon: rectangle, title: "Here are some things you should know regarding how we work", id: 2 },
-        { icon: rectangle, title: "Here are some things you should know regarding how we work", id: 3 },
+        { icon: rectangle13, title: "Here are some things you should know regarding how we work", id: 2 },
+        { icon: rectangleOrange, title: "Here are some things you should know regarding how we work", id: 3 },
 
-        { icon: rectangle, title: "Here are some things you should know regarding how we work", id: 4 },
-        { icon: rectangle, title: "Here are some things you should know regarding how we work", id: 5 },
-        { icon: rectangle, title: "Here are some things you should know regarding how we work", id: 6 },
-
-
-        { icon: rectangle, title: "Here are some things you should know regarding how we work", id: 7 },
-        { icon: rectangle, title: "Here are some things you should know regarding how we work", id: 8 },
-        { icon: rectangle, title: "Here are some things you should know regarding how we work", id: 9 },
+        { icon: rectangle16, title: "Here are some things you should know regarding how we work", id: 4 },
+        { icon: rectangle14, title: "Here are some things you should know regarding how we work", id: 5 },
+        { icon: rectangle15, title: "Here are some things you should know regarding how we work", id: 6 },
 
 
-        { icon: rectangle, title: "Here are some things you should know regarding how we work", id: 10 },
-        { icon: rectangle, title: "Here are some things you should know regarding how we work", id: 11 },
-        { icon: rectangle, title: "Here are some things you should know regarding how we work", id: 12 }
+        { icon: rectangle17, title: "Here are some things you should know regarding how we work", id: 7 },
+        { icon: rectangle18, title: "Here are some things you should know regarding how we work", id: 8 },
+        { icon: rectangle19, title: "Here are some things you should know regarding how we work", id: 9 },
+
+
+        { icon: rectangle20, title: "Here are some things you should know regarding how we work", id: 10 },
+        { icon: rectangle21, title: "Here are some things you should know regarding how we work", id: 11 },
+        { icon: rectangle22, title: "Here are some things you should know regarding how we work", id: 12 }
     ])
 
     return (
-        <div className="lg:w-full lg:h-full grid grid-cols-2 gap-2 p-4 pl mb-2 ">
+        <div className="lg:w-full lg:h-full grid grid-cols-2  pl mb-2 ">
 
             {blogs.map((blog) => (
-                <div className="blogGrid grid place-content-center p-4">
+                <div className="blogGrid grid place-content-center p-4" key={blog.id}>
                     <div className="blogCard w-96 bg-white border border-gray-200 rounded-lg shadow bg-gray-100 dark:border-gray-700">
                         <a href="#">
                             <img class="rounded-t-lg" src={blog.icon} alt="" style={{ width: "100%" }} />
